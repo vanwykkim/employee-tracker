97fef9d9e2facbcb834a3c83d9a0da6965ed40d7
Kim Van Wyk <kmvanwyk@msn.com>
2022-12-22 18:35:00 -0800

    added schema.sql:

---
+++ main/db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL.
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
